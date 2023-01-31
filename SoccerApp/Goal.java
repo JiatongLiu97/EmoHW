c7eaeca1e01e25bd83acf4536e74a0efaec68356
JiatongLiu97 <jiatongliu97@outlook.com>
2023-01-31 01:04:32 -0800

    Initial commit

---
+++ SoccerApp/Goal.java
@@ -0,0 +1,12 @@
+
+public class Goal {
+	public Team team;
+	public Player player;
+	public double time;
+	
+	public Goal(Team team, Player player, double time) {
+		this.team = team;
+		this.player = player;
+		this.time = time;
+	}
+}
